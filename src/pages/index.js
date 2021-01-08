0fd4e259cfcfa28546cf4d93039a0cd01d1187f6
naila <nailaparwin@gmail.com>
2021-01-08 22:19:27 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react"
 import Header from './header'
 import Desc from './desc'
-
+import Grid from '@material-ui/core/Grid';
 export default function Home() {
   return (
     <div>
+       <Grid container spacing={1}>
+      <Grid item xs={12} sm={12} md={12}>
       <Header/>
+      </Grid>
+      <Grid item xs={12} sm={12} md={12}>
       <Desc/>
+      </Grid></Grid>
     </div>
   )
 }
