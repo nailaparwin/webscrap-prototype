39454dba16fea4fa0e64ca398b5371541fd16f67
Roomi <nailaparwin@gmail.com>
2021-01-22 00:19:01 +0500

    msg

--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -3,6 +3,7 @@ import { ApolloProvider } from '@apollo/client';
 import { client } from './client';
 
 //comment from code push
+//new commit
 export const wrapRootElement = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
 );
