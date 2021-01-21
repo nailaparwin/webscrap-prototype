75855fc7a21f31b34af7353d957521baf14bc9b7
Roomi <nailaparwin@gmail.com>
2021-01-21 23:38:04 +0500

    hello

--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './client';
 
+//comment from code push
 export const wrapRootElement = ({ element }) => (
   <ApolloProvider client={client}>{element}</ApolloProvider>
 );
