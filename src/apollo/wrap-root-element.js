f431d6478764f79af58d652bb2e7bad8fa6d5775
Roomi <nailaparwin@gmail.com>
2021-01-22 02:26:58 +0500

    new commit

--- src/apollo/wrap-root-element.js
+++ src/apollo/wrap-root-element.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './client';
-
-//comment from code push
-//new commit
-export const wrapRootElement = ({ element }) => (
-  <ApolloProvider client={client}>{element}</ApolloProvider>
-);
-
-
+add new data
